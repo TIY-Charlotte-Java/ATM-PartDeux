f4a569c575c551be753db3b5087f95b03c3d1575
Ben Sterrett <raynjamin@gmail.com>
2016-03-30 12:16:46 -0400

    Adding hi.

---
+++ src/com/theironyard/clt/Main.java
@@ -0,0 +1,10 @@
+package com.theironyard.clt;
+
+/**
+ * Created by Ben on 3/30/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
