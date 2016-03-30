7cbdb9ed501c435a698ee91ee2f458fd0a7d92f7
Ben Sterrett <raynjamin@gmail.com>
2016-03-30 12:20:49 -0400

    Adding some comments

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -5,6 +5,14 @@ package com.theironyard.clt;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        // This is the starting point of your program
+        // Architect your solution however you see fit.
+
+        // quick note: throwing exceptions
+        // you can throw a new one with this:
+        // throw new IllegalArgumentException("Message About Exception");
+
+        // You will need this in a couple places in your code.
+        // We will discuss more about exceptions in the coming days.
     }
 }
