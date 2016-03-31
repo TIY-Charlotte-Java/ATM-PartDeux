aaff6b92ad9d4bc3789896062ef171c7ea044658
Michael Grill <Ultramar@michaels-mbp.home>
2016-03-30 21:58:37 -0400

    lol

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -1,16 +1,39 @@
 package com.theironyard.clt;
 
-/**
- * Created by Ben on 3/30/16.
- */
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
 
         System.out.println("Hello, please enter your name.");
         Scanner scanner = new Scanner(System.in);
 
         String name = scanner.nextLine();
-        
+        System.out.println("Welcome, " + name);
+
+        System.out.println("Choose a function, Balance, Withdraw, or Cancel");
+        String option = scanner.nextLine();
+
+        if (option.equalsIgnoreCase("balance")) {
+            System.out.println("Your balance is $100.");
+
+        } else if (option.equalsIgnoreCase("withdraw")) {
+            System.out.println("How much?");
+
+            int amount = scanner.nextInt();
+            if (amount > 100) {
+                System.out.println("Insufficient funds");
+
+            } else if (amount <= 0) {
+                System.out.println("Positive amount needed.");
+
+            } else {
+                System.out.println("Withdraw complete, please take your money.");
+
+            }
+        }
+
 
 
 
