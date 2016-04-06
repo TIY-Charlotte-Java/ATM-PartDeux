d77e0744021c508702b4c0d0290813c1a30d7e8e
crenny288 <crenny288@gmail.com>
2016-04-06 09:40:42 -0400

    ATM partTwo

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -1,6 +1,8 @@
+
 package com.theironyard.clt;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,62 +11,71 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    //main method
+
+    public  static void main(String[] args) {
         displayMenu();
-    }
-
-    private static int makeSelection(ArrayList<Integer> options) {
-        Scanner input = new Scanner(System.in);
-
-        int choice = -1;
-
-        try {
-            choice = input.nextInt();
-        }
-
-        catch (InputMismatchException ex) {
-
-        }
-
-        if (options.contains(choice) == false {
-            choice = -1;
-            System.out.println("Invalid Selection.");
-        }
-
-        return choice;
 
     }
 
-     public static void displayMenu() {
-         int selection = 0;
+    // display menu method
 
-         ArrayList<Integer> options = new ArrayList<>();
+    private static void displayMenu() {
+        int selection = 0;
 
-         options.add(1);
-         options.add(2);
-         options.add(3);
+        ArrayList<Integer> options = new ArrayList<>();
 
-         do {
-             System.out.println("1. Check Balance");
-             System.out.println("2. Withdraw Funds");
-             System.out.println("3. Deposi Money");
+        options.add(1);
+        options.add(2);
+        options.add(3);
 
-             selection = makeSelection(options);
-         } while (selection == -1);
+        do {
+            System.out.println("\n\n       Bank Account Menu");
+            System.out.println("##############################");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdraw Funds");
+            System.out.println("3. Deposit Money");
+            System.out.println("\nSelect a Menu Option: ");
 
-         if (selection == 1) {
-             //checkBalance ();
-             System.out.println("Checked Balance");
+            selection = makeSelection(options);
+        } while (selection == -1);
 
-         } else if (selection == 2) {
-             //withdraw
-             System.out.println("Withdrew Money");
-         }
+        if (selection == 1) {
+            // checkBalance();
+            System.out.println("Checked Balance");
+        } else if (selection == 2) {
+            // withdraw
+            System.out.println("Withdrew Money");
+        } else if (true) {
+            // deposit
+            System.out.println("Deposited Cash");
 
-
-
-
-
+        }
+        while (true); // Display the menu until the user closes the program
 
     }
+
+    //input method
+
+        private static int makeSelection(ArrayList<Integer> options) {
+            Scanner input = new Scanner(System.in);
+
+            int choice = -1;
+
+            try {
+                choice = input.nextInt();
+            }
+
+            catch (InputMismatchException ex) {
+            }
+
+            if (options.contains(choice) == false) {
+                choice = -1;
+                System.out.println("Invalid Selection.");
+            }
+
+            return choice;
+    }
+
+
 }
--- src/com/theironyard/clt/NewAccount.java
+++ src/com/theironyard/clt/NewAccount.java
@@ -1,4 +1,4 @@
-package com.theironyard.clt;
+//package com.theironyard.clt;
 
 import javax.security.auth.login.AccountException;
 import java.util.HashMap;
@@ -11,9 +11,13 @@ public class NewAccount {
     public static void main(String args[]) {
 
         Scanner input = new Scanner(System.in);
-        HashMap<Integer, AccountException> accountExceptionHashMap = new HashMap<Integer, AccountException>();
+        HashMap<String, Double> map = new HashMap<>();
 
-            //Options
+        map.put("Michael", 200.00);
+        map.put("Anthony", 300.00);
+        map.put("Ben", 400.00);
+
+        //Options
         while (true) {
 
             System.out.println("Enter the option for the operation you need;");
@@ -25,77 +29,10 @@ public class NewAccount {
             System.out.println("> ");   //indicator for user input
 
             String choice = input.next();
-            System.out.println();("Your choice; " + choice);
-
-            if(choice.equals("ne")) {
-                Integer newAccountNumber;
-                Double intialBalance;
-                AccountException newAccount;
-
-                // Array for account and balance
-                System.out.print("Insert account number: ");
-                newAccountNumber = input.nextInt(); //-- Input nr array insertion
-                System.out.print("Enter initial balance: ");
-                intialBalance=input.nextDouble(); //Input nr array insertion
-                newAccount = new AccountException(newAccountNumber, intialBalance);
-                accountExceptionHashMap.put(newAccountNumber, newAccount);
-                System.out.println("New Account " + newAccountNumber + " created with balance: " + intialBalance);
-
-            }
-            // select account
-            else if(choice.equals("se")) {
-                System.out.println("Enter number of account to be selected: ");
-                Integer accounttoGetNumber = input.nextInt();
-                AccountException returnedAccount = accountExceptionHashMap.get(accounttoGetNumber);
-                if (returnedAccount != null)
-                {
-                    System.out.println("Account open. Current balance: " + returnedAccount.getBalance());
-                }
-                else
-                {
-                    //user input for account nr from array
-                    System.out.println("Account does not exist.");
-                }
-            }
+            System.out.println("Your choice; " + choice);
 
         }
-        //close account
-        else if (choice.equals("de"))
-        {
-            System.out.println("Enter number of account to be selected: ");
-            Integer accountToDeleteNumber = input.nextInt();
-            AccountException removedAccount = accountExceptionHashMap.remove(accountToDeleteNumber);
-            if (removedAccount != null)
-            {
-                System.out.println("Account " + removedAccount.getAccountNumber() + "has been closed with balance: " + removedAccount.geBalance());
-            }
-            else
-            {
-                //user input for account nr from array
-                System.out.println("Account does not exist.");
-            }
-        }
-        // deposit
-        else if (choice.equals("dp")) {
-            System.out.println("Enter number of account to deposit: ");
-            Integer accountToDeposit = input.nextInt();
-            System.out.print("Enter ammount to deposit: ");
-            double amount = input.nextDouble();
-            if(amount <= 0){
-                System.out.println("You must deposit an amount greater than 0.");
-            } else {
-                accountExceptionHashMap.get(accountToWithdraw).withdraw(amount);
-                System.out.println();("You have deposited " + (amount));
-                System.out.println("Current balance " + accountExceptionHashMap.get(accountToWithdraw).getBalance());
-            }
-        }
-        //quit
-        else if (choice.equals("ex")) {
-            break;
-        } else {
-            System.out.println("Wrong option.");
-        } //end of loop
+    }
 
-        input.close();
-    } //end of main
-} //end of class
+}
+
